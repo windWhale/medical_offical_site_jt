de37f802f11f3a44c50422befc00a781dc070796
wik_J <18839112278@163.com>
2021-11-09 23:37:06 +0800

    navbar完成

--- src/Layout/HomeLayout.tsx
+++ src/Layout/HomeLayout.tsx
@@ -1,37 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+// import { Link } from "react-router-dom";
 
-import { routerList } from '../common/constant';
+import { routerList } from "../common/constant";
 
-import styles from './index.module.less';
+import "./index.less";
 
 interface IHomeLayoutProps {
     children: JSX.Element | string;
-};
+}
 
 export default function HomeLayout(props: IHomeLayoutProps) {
+    useEffect(() => {
+        // header滚轮事件
+        window.addEventListener("scroll", (e) => {
+            let header = document.querySelector("header");
+            header.classList.toggle("sticky", window.scrollY > 0);
+        });
+    }, []);
     return (
-        <div className={styles['layout-container']}>
-            <header className={styles['header']}>
-                <h1>LOGO</h1>
-                <label htmlFor="toggle-nav" className={styles['menuBtn']}></label>
-                <input type="checkbox" name="" id="toggle-nav"/>
-                <div className={styles['collapse']}>
+        <div className="layout-container">
+            <header className="header">
+                <div className="logo"></div>
+                <label
+                    htmlFor="toggle-nav"
+                    className="menuBtn"
+                ></label>
+                <input type="checkbox" name="" id="toggle-nav" />
+                <div className="collapse">
                     <nav>
-                        {
-                            routerList.map(item => (
-                                <a key={item.code} href={`#/${item.code}`}>{item.title}</a>
-                            ))
-                        }
+                        {routerList.map((item) => (
+                            <a key={item.code} href={`#/${item.code}`}>
+                                {item.title}
+                            </a>
+                        ))}
                     </nav>
                 </div>
             </header>
-            <section className={styles['content']}>
-                {props.children}
-            </section>
-            <footer className={styles['footer']}>
-                footer businessinfo
-            </footer>
+            <section className="content">{props.children}</section>
+            <footer className="footer">footer businessinfo</footer>
         </div>
-    )
+    );
 }
